e7c2c6e6364bd55d3c78aca9cd0fd6eee8eb9d4f
tunnckoCore <mameto_100@mail.bg>
2015-05-11 11:26:10 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-/**
+/*!
  * is-empty-function <https://github.com/tunnckoCore/is-empty-function>
  *
- * Copyright (c) 2014-2015 Charlike Mike Reagent, contributors.
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
  * Released under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-var balanced = require('balanced-match');
-var cleanupCoverageCode = require('cleanup-coverage-code');
+'use strict'
+
+var balanced = require('balanced-match')
+var cleanupCoverageCode = require('cleanup-coverage-code')
 
 /**
  * Check given function have empty body or not, and returns true or false.
@@ -16,22 +18,22 @@ var cleanupCoverageCode = require('cleanup-coverage-code');
  * **Example:**
  *
  * ```js
- * var isEmptyFunction = require('is-empty-function');
+ * var isEmptyFunction = require('is-empty-function')
  *
- * var fixture = 'function() {}';
- * isEmptyFunction(fixture);
+ * var fixture = 'function() {}'
+ * isEmptyFunction(fixture)
  * //=> true
  *
- * var fixture = function named() {};
- * isEmptyFunction(fixture);
+ * var fixture = function named() {}
+ * isEmptyFunction(fixture)
  * //=> true
  *
- * var fixture = function() { return true; };
- * isEmptyFunction(fixture);
+ * var fixture = function() { return true; }
+ * isEmptyFunction(fixture)
  * //=> false
  *
- * var fixture = function named() { return true; };
- * isEmptyFunction(fixture);
+ * var fixture = function named() { return true; }
+ * isEmptyFunction(fixture)
  * //=> false
  * ```
  *
@@ -40,12 +42,12 @@ var cleanupCoverageCode = require('cleanup-coverage-code');
  * @return {Boolean}
  * @api private
  */
-module.exports = function isEmptyFunction(fn) {
+module.exports = function isEmptyFunction (fn) {
   if (fn) {
-    var fnString = fn.toString();
-    var body = balanced('{', '}', fnString).body;
-    body = cleanupCoverageCode(body);
-    return body.length > 0 ? false : true;
+    var fnString = fn.toString()
+    var body = balanced('{', '}', fnString).body
+    body = cleanupCoverageCode(body)
+    return body.length > 0 ? false : true
   }
-  return false;
-};
+  return false
+}
