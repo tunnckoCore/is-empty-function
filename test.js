1732584ef54283598f97e94996cbfda761ac736c
tunnckoCore <mameto_100@mail.bg>
2016-01-29 16:32:07 +0200

    update tests

--- test.js
+++ test.js
@@ -12,120 +12,39 @@
 var test = require('assertit')
 var isEmptyFunction = require('./index')
 
-test('the body is not empty (including when arrow function)', function (done) {
-  var arrowFnStr = '(j, k) => {\n  var foo = 1\n  return j + k}'
-  var fnStr = 'function named (j, k) {\n  var foo = 1\n  return j + k}'
+test('body is not empty', function (done) {
+  test.strictEqual(isEmptyFunction('function () { return true }'), false)
+  test.strictEqual(isEmptyFunction('function c (d) {return d} foo'), false)
+  test.strictEqual(isEmptyFunction(function () { return true }), false)
+  test.strictEqual(isEmptyFunction(function (a) { return a }), false)
+  test.strictEqual(isEmptyFunction('z => { return z * 3 }'), false)
+  test.strictEqual(isEmptyFunction('(a, b) => { return a * b}'), false)
+  test.strictEqual(isEmptyFunction('(x, y) => x + 2 + y'), false)
 
-  test.strictEqual(isEmptyFunction(arrowFnStr), false)
-  test.strictEqual(isEmptyFunction(fnStr), false)
+  // on enviroment which support arrow functions
+  // test.strictEqual(isEmptyFunction(z => { return z * 3 }), false)
+  // test.strictEqual(isEmptyFunction((a, b) => { return a * b}), false)
+  // test.strictEqual(isEmptyFunction((x, y) => x + 2 + y), false)
   done()
 })
 
-test('empty body (including when arrow function)', function (done) {
-  var emptyArrow = '(a) => {}'
-  var emptyFn = 'function named (a) {}'
+test('body is empty', function (done) {
+  var fixture = "function codeCov (a) {__cov_Ejgcx$XN18CSfmeWn$f7vQ.f['2']++;}"
 
-  test.strictEqual(isEmptyFunction(emptyArrow), true)
-  test.strictEqual(isEmptyFunction(emptyFn), true)
-  done()
-})
+  test.strictEqual(isEmptyFunction(fixture), true)
+  test.strictEqual(isEmptyFunction('function () {}'), true)
+  test.strictEqual(isEmptyFunction(function () {}), true)
+  test.strictEqual(isEmptyFunction('function (a) {}'), true)
+  test.strictEqual(isEmptyFunction(function (b) {}), true)
+  test.strictEqual(isEmptyFunction('function c (d) {}'), true)
+  test.strictEqual(isEmptyFunction(function x (y) {}), true)
+  test.strictEqual(isEmptyFunction('z => {}'), true)
+  test.strictEqual(isEmptyFunction('(foo, bar) => {}'), true)
+  test.strictEqual(isEmptyFunction('() => {}'), true)
+  test.strictEqual(isEmptyFunction('function c (d) {} foo'), true)
 
-// old tests below
-test('should return `true` when empty anonymous function given', function (done) {
-  var fixture = function () {}
-  var actual = isEmptyFunction(fixture)
-  var expected = true
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
-  done()
-})
-
-test('should return `true` when empty named function given', function (done) {
-  var fixture = function named () {}
-  var actual = isEmptyFunction(fixture)
-  var expected = true
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
-  done()
-})
-
-test('should return `true` when empty anonymous function in string given', function (done) {
-  var fixture = 'function () {}'
-  var actual = isEmptyFunction(fixture)
-  var expected = true
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
-  done()
-})
-
-test('should return `true` when empty named function in string given', function (done) {
-  var fixture = 'function named () {}'
-  var actual = isEmptyFunction(fixture)
-  var expected = true
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
-  done()
-})
-
-test('should return `true` when named function contains coverage code', function (done) {
-  var fixture = 'function named () {}'
-  var actual = isEmptyFunction(fixture)
-  var expected = true
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
-  done()
-})
-
-test('should return `false` when no arguments given', function (done) {
-  var actual = isEmptyFunction()
-  var expected = false
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
-  done()
-})
-
-test('should return `false` when anonymous function with no empty body is given', function (done) {
-  var fixture = function () { return true }
-  var actual = isEmptyFunction(fixture)
-  var expected = false
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
-  done()
-})
-
-test('should return `false` when anonymous function in string with no empty body is given', function (done) {
-  var fixture = 'function() { return true }'
-  var actual = isEmptyFunction(fixture)
-  var expected = false
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
-  done()
-})
-
-test('should return `false` when named function with no empty body is given', function (done) {
-  var fixture = function named () { return true }
-  var actual = isEmptyFunction(fixture)
-  var expected = false
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
-  done()
-})
-
-test('should return `false` when named function in string with no empty body is given', function (done) {
-  var fixture = 'function named () { return true }'
-  var actual = isEmptyFunction(fixture)
-  var expected = false
-
-  test.strictEqual(typeof actual, 'boolean')
-  test.strictEqual(actual, expected)
+  // on enviroment which support arrow functions
+  // test.strictEqual(isEmptyFunction(z => {}), true)
+  // test.strictEqual(isEmptyFunction((a, b) => {}), true)
   done()
 })
